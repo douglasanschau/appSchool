3794ff0b748e6eb656ea61271b6adfe957b7b58b
Douglas Anschau Flores <76949311+douglasanschau@users.noreply.github.com>
2023-11-12 13:18:46 -0300

    Delete src directory

--- src/components/DataTableStudents/index.js
+++
@@ -1,9 +0,0 @@
-import  DataTable from 'react-data-table-component';
-
-function DataTableStudents(title, columns, data, pending){
-    return (
-        <DataTable title={title} columns={columns} data={data} sortactive progressPending={pending}  pagination/>
-    );
-}
-
-export default DataTableStudents;
